0e2cda13c9a902f6afd686212efa6fc934742c8b
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-11-01 23:42:58 +0900

    AddEdit

---
+++ app/src/main/java/com/amebaownd/pikohan_nwiatori/noticemeplz/data/repository/AddEditRepository.kt
@@ -0,0 +1,36 @@
+package com.amebaownd.pikohan_nwiatori.noticemeplz.data.repository
+
+import com.amebaownd.pikohan_nwiatori.noticemeplz.data.dao.AddEditUserDao
+import com.amebaownd.pikohan_nwiatori.noticemeplz.data.model.User
+import com.amebaownd.pikohan_nwiatori.noticemeplz.data.model.UserAndUsingService
+import com.amebaownd.pikohan_nwiatori.noticemeplz.data.model.UsingService
+
+class AddEditRepository (private val addEditUserDao: AddEditUserDao){
+
+    fun getById(userId:String):UserAndUsingService{
+        return addEditUserDao.getById(userId)
+    }
+
+    suspend fun insertUser(user:User){
+        addEditUserDao.insertUser(user)
+    }
+    suspend fun insertUsingService(vararg usingService: UsingService){
+        addEditUserDao.insertUsingService(*usingService)
+    }
+
+    suspend fun updateUser(user: User){
+        addEditUserDao.updateUser(user)
+    }
+
+    suspend fun updateUsingService(vararg usingService: UsingService){
+        addEditUserDao.updateUsingService(*usingService)
+    }
+
+    suspend fun deleteUser(user: User){
+        addEditUserDao.deleteUser(user)
+    }
+
+    suspend fun deleteUsingService(usingService: UsingService){
+        addEditUserDao.deleteUsingService(usingService)
+    }
+}
