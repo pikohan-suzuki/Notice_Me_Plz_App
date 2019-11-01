7687fb3aca36b3c10ef5b0f9d529243dd99f4e85
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-11-01 23:45:12 +0900

    app

---
+++ app/src/main/java/com/amebaownd/pikohan_nwiatori/noticemeplz/util/NoticeMePlzApp.kt
@@ -0,0 +1,53 @@
+package com.amebaownd.pikohan_nwiatori.noticemeplz.util
+
+import android.app.Application
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.os.Build
+import java.lang.RuntimeException
+
+//class NoticeMePlzApp : Application() {
+//    val repository: FoodStuffRepository
+//        get() = ServiceLoader.provideFoodStuffRepository(
+//            this
+//        )
+//    override fun onCreate() {
+//        super.onCreate()
+//        MyContext.onCreateAppcalition(this)
+//    }
+//}
+
+class MyContext(application : Context){
+    var applicationContext = application
+    companion object{
+        private lateinit var instance:MyContext
+        fun onCreateAppcalition(applicationContext: Context){
+            instance = MyContext(applicationContext)
+        }
+        fun getContext(): Context {
+            if(instance.applicationContext==null)
+                throw RuntimeException()
+            return instance.applicationContext
+        }
+        fun getString(resId:Int):String{
+            return instance.applicationContext.getString(resId)
+        }
+
+        fun getColor(resId:Int):Int{
+            return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                instance.applicationContext.getColor(resId)
+            } else {
+                instance.applicationContext.resources.getColor(resId)
+            }
+        }
+
+        fun getDrawable(resId:Int):Drawable?{
+            return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                instance.applicationContext.getDrawable(resId)
+            } else {
+                instance.applicationContext.resources.getDrawable(resId)
+            }
+        }
+
+    }
+}
