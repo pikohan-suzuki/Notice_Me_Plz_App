ab5acde8fc78cbdbb66bae028fa0f061f753f166
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-11-04 15:21:31 +0900

    delete unused function

--- app/src/main/java/com/amebaownd/pikohan_nwiatori/noticemeplz/util/RecyclerViewBinding.kt
+++ app/src/main/java/com/amebaownd/pikohan_nwiatori/noticemeplz/util/RecyclerViewBinding.kt
@@ -6,25 +6,8 @@ import com.amebaownd.pikohan_nwiatori.noticemeplz.addEditUser.AddEditUserAdapter
 import com.amebaownd.pikohan_nwiatori.noticemeplz.data.model.Message
 import com.amebaownd.pikohan_nwiatori.noticemeplz.data.model.UserAndUsingService
 import com.amebaownd.pikohan_nwiatori.noticemeplz.data.model.UsingService
+import com.amebaownd.pikohan_nwiatori.noticemeplz.talk.TalkAdapter
 import com.amebaownd.pikohan_nwiatori.noticemeplz.userList.UserListAdapter
-
-//@BindingAdapter("app:foodstuff_items")
-//fun setFoodStuffItems(listView: RecyclerView, items:List<FoodStuff>?){
-//    if(items != null)
-//        (listView.adapter as FoodStuffsAdapter).submitList(items)
-//}
-//
-//@BindingAdapter("app:dish_items")
-//fun setDishItems(listView: RecyclerView, items:List<Dish>?){
-//    if(items!=null)
-//        (listView.adapter as DishAdapter).submitList(items)
-//}
-//
-//@BindingAdapter("app:added_foodstuff_items")
-//fun setAddEditItems(listView: RecyclerView, items:List<AddedFoodStuff>?){
-//    if(items!=null)
-//        (listView.adapter as AddEditDishAdapter).submitList(items)
-//}
 
 @BindingAdapter("app:add_edit_items")
 fun setAddEditItems(listView:RecyclerView,items:List<UsingService>?){
@@ -38,8 +21,8 @@ fun setUserListItems(listView:RecyclerView,items:List<UserAndUsingService>?){
         (listView.adapter as UserListAdapter).submitList(items)
 }
 
-//@BindingAdapter("app:talk_items")
-//fun setTalkItems(listView:RecyclerView,items:List<Message>?){
-//    if(items!=null)
-//        (listView.adapter as Talk).submitList(items)
-//}
+@BindingAdapter("app:talk_items")
+fun setTalkItems(listView:RecyclerView,items:List<Message>?){
+    if(items!=null)
+        (listView.adapter as TalkAdapter).submitList(items)
+}
